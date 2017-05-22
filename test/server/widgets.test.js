e6b3be39870dbf7237277f6181a10e2330fccda7
Joshua Vial <joshua@enspiral.com>
2017-05-22 21:28:00 +1200

    testing post route of server

--- test/server/widgets.test.js
+++ test/server/widgets.test.js
@@ -5,13 +5,27 @@ import app from '../../server/server'
 
 //the server keeps the widgets in memory so no knex setup neede
 
-test.cb('GET /widgets', t => {
+test.serial.cb('GET /widgets', t => {
   request(app)
     .get('/widgets')
     .expect(200)
     .end((err,res) => {
       t.is(res.body.length, 3)
       t.end()
+    })
+})
+
+test.serial.cb('POST /widgets', t => {
+  request(app)
+    .post('/widgets')
+    .send({name:'test'})
+    .expect(200)
+    .end((err, res) => {
+      request(app)
+        .get('/widgets').end((err, res) => {
+          t.is(res.body.length, 4)
+          t.end()
+        })
     })
 })
 
