728323493e8251bba03aa33b73274af5ee4c15d6
emily <emilycoco@me.com>
2018-11-14 19:39:58 +1300

    db path

--- server/routes/widgets.js
+++ server/routes/widgets.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
-const db = require('../db')
+const db = require("../db/db")
 
-router.get('/', function (req, res) {
-  res.json(db.getWidgets())
+router.get("/", function(req, res) {
+  db.getWidgets()
+    .then(widgets => {
+      res.send(widgets)
+    })
+    .catch(err => {
+      res.status(500).send(err.message)
+    })
 })
 
-router.post('/', function (req, res) {
+router.post("/", function(req, res) {
   db.saveWidget(req.body)
   // Normally saveWidget would be async and you'd
   // have to sendStatus in the .then, but in this
