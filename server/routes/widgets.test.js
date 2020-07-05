dc3d281a515f9cc7ed5f2371ee9850afba9ca2fd
Don Smith <hi@dons.dev>
2020-07-06 05:53:44 +1200

    mock the database module

--- server/routes/widgets.test.js
+++ server/routes/widgets.test.js
@@ -1,11 +1,19 @@
 import request from 'supertest'
 
-import app from '../../server/server'
+import server from '../../server/server'
 
-// the server keeps the widgets in memory so no knex setup needed
+jest.mock('../db/db', () => {
+  return {
+    getWidgets: () => Promise.resolve([
+      { id: 1, name: 'test 1', price: 1.23, mfg: 'Test 1 Inc.', inStock: 4 },
+      { id: 2, name: 'test 2', price: 45.67, mfg: 'Test 2 Inc.', inStock: 0 },
+      { id: 3, name: 'test 3', price: 890.12, mfg: 'Test 3 Inc.', inStock: 8 }
+    ])
+  }
+})
 
 test('GET /api/v1/widgets', () => {
-  return request(app)
+  return request(server)
     .get('/api/v1/widgets')
     .expect(200)
     .then(res => {
