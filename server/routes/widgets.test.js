4081bbde93c81bb6214a8a4c5ae6eac3f872f110
Lache Melvin <lache.melvin@gmail.com>
2020-12-16 18:33:17 +1300

    test the route catch block

--- server/routes/widgets.test.js
+++ server/routes/widgets.test.js
@@ -1,23 +1,34 @@
 import request from 'supertest'
 
-import server from '../../server/server'
+import server from '../server'
+import { getWidgets } from '../db/db'
 
-jest.mock('../db/db', () => {
-  return {
-    getWidgets: () => Promise.resolve([
+jest.mock('../db/db')
+
+describe('GET /api/v1/widgets', () => {
+  it('responds with widgets array on getWidgets success', () => {
+    getWidgets.mockImplementation(() => Promise.resolve([
       { id: 1, name: 'test 1', price: 1.23, mfg: 'Test 1 Inc.', inStock: 4 },
       { id: 2, name: 'test 2', price: 45.67, mfg: 'Test 2 Inc.', inStock: 0 },
       { id: 3, name: 'test 3', price: 890.12, mfg: 'Test 3 Inc.', inStock: 8 }
-    ])
-  }
-})
-
-test('GET /api/v1/widgets', () => {
-  return request(server)
-    .get('/api/v1/widgets')
-    .expect(200)
-    .then(res => {
-      expect(res.body).toHaveLength(3)
-      return null
-    })
+    ]))
+    return request(server)
+      .get('/api/v1/widgets')
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toHaveLength(3)
+        expect(res.body[1].price).toBe(45.67)
+        return null
+      })
+  })
+  it('responds with 500 and error on getWidgets rejection', () => {
+    getWidgets.mockImplementation(() => Promise.reject(new Error('mock DB error')))
+    return request(server)
+      .get('/api/v1/widgets')
+      .expect(500)
+      .then((err) => {
+        expect(err.text).toBe('mock DB error')
+        return null
+      })
+  })
 })
