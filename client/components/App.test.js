2f4585b7f27dff3a16725369830a8c26629dcbb7
Lache Melvin <lache.melvin@gmail.com>
2020-12-16 18:50:58 +1300

    remove leftover complex App testing

--- client/components/App.test.js
+++ client/components/App.test.js
@@ -1,47 +1,14 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import user from '@testing-library/user-event'
 
-import App from '../../client/components/App'
-
-jest.mock('../api', () => ({
-  getWidgets: () => Promise.resolve([
-    { id: 1, name: 'Red widget', price: 23.45, mfg: 'Acme Inc.', inStock: 4 },
-    { id: 2, name: 'Blue widget', price: 423.47, mfg: 'Acme Inc.', inStock: 8 },
-    { id: 3, name: 'Yellow widget', price: 123.40, mfg: 'Acme Inc.', inStock: 3 }
-  ])
-}))
+import App from './App'
 
 test('Shows heading', () => {
   render(<App />)
   return screen.findAllByRole('heading')
     .then(headings => {
       expect(headings[0]).toHaveTextContent('Widgets FTW!')
-      return null
-    })
-})
-
-test('Renders widget list', () => {
-  render(<App />)
-  return screen.findAllByRole('listitem')
-    .then(items => {
-      expect(items).toHaveLength(3)
-      return null
-    })
-})
-
-test('Shows widget details', () => {
-  render(<App />)
-  const details = screen.queryByRole('heading', { name: 'Details' })
-  expect(details).toBeNull()
-  return screen.findAllByRole('link')
-    .then(links => {
-      user.click(links[1])
-      return screen.findByRole('heading', { name: 'Details' })
-    })
-    .then(detailsHeader => {
-      expect(detailsHeader).toBeInTheDocument()
       return null
     })
 })
