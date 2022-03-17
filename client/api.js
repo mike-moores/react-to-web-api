51679ebd62b314c2fd8f2151340867e02335d290
ahmad <eng.ahmed.anwar@gmail.com>
2022-03-18 08:17:04 +1300

    remove getWidgets implementation and update instructions

--- client/api.js
+++ client/api.js
@@ -2,6 +2,4 @@ import request from 'superagent'
 
 const widgetUrl = '/api/v1/widgets/'
 
-export function getWidgets() {
-  return request.get(widgetUrl).then((response) => response.body)
-}
+export function getWidgets() {}
