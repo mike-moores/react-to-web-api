62dfdad9e83a978b47864ba67edd062e51ac015a
Gerard Paapu <gerard@gerardpaapu.com>
2022-05-02 10:43:14 +1200

    Bring solution packages up to lint

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,5 +1,6 @@
+// eslint-disable-next-line no-unused-vars
 import request from 'superagent'
-
+// eslint-disable-next-line no-unused-vars
 const widgetUrl = '/api/v1/widgets/'
 
 export function getWidgets() {}
