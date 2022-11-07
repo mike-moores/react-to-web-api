220d25803277b642a0df9f67a21229144a78d380
Sophia Lawler <sophialawler@sophias-mbp.lan>
2022-11-08 12:21:50 +1300

    fix tests with ts mock

--- server/db/db.ts
+++ server/db/db.ts
@@ -2,7 +2,7 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
-export type TWidget = {
+export interface TWidget {
   id: number
   name: string
   price: number
@@ -10,6 +10,6 @@ export type TWidget = {
   inStock: number
 }
 
-export function getWidgets(db = connection): Promise<Array<TWidget>> {
+export function getWidgets(db = connection): Promise<TWidget[]> {
   return db('widgets').select()
 }
