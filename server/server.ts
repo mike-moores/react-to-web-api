3c887155e0473e8e6add1aaf70931d50c768a499
sophia-lawler <sophialawler@gmail.com>
2022-12-02 15:23:29 +1300

    destructure join import

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,11 @@
-import path from 'path'
+import { join } from 'node:path'
 import express from 'express'
 
 const widgets = require('./routes/widgets')
 
 const server = express()
 server.use(express.json())
-server.use(express.static(path.join(__dirname, 'public')))
+server.use(express.static(join(__dirname, 'public')))
 
 server.use('/api/v1/widgets', widgets)
 
