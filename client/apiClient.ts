ebca44721f58d7bfbc24d6a8176a271758a34940
Kelly Keating <keating.kel@gmail.com>
2023-02-02 11:22:56 +1300

    match type style

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import request from 'superagent'
-import Widget from '../common/Widget'
+
 const widgetUrl = '/api/v1/widgets/'
 
-export function getWidgets() {}
+export function getWidgets(): void {}
