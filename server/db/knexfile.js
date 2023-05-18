5c2fe34a4b77e3f8ab35aa0cc88fab079afff529
Gerard Paapu <gerard@gerardpaapu.com>
2023-05-18 16:31:06 +1200

    Align knexfiles

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -1,11 +1,11 @@
-const path = require('path')
+const { join } = require('node:path')
 
 module.exports = {
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: path.join(__dirname, 'dev.sqlite3'),
+      filename: join(__dirname, 'dev.sqlite3'),
     },
     pool: {
       afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
@@ -19,10 +19,10 @@ module.exports = {
       filename: ':memory:',
     },
     migrations: {
-      directory: path.join(__dirname, 'migrations'),
+      directory: join(__dirname, 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, 'seeds'),
+      directory: join(__dirname, 'seeds'),
     },
     pool: {
       afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
@@ -30,18 +30,13 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
+      filename: '/app/storage/prod.sqlite3',
     },
     pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
     },
   },
 }
