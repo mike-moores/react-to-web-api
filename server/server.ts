02f96bb92f36e8d52f5961c9a49aa5e61522b1bf
Jen Hurley <j.s.hurley00@gmail.com>
2023-08-11 12:21:21 +1200

    update __dirname in server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,15 @@
-import { join } from 'node:path'
+import * as Path from 'node:path'
+import * as URL from 'node:url'
+
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
+
 import express from 'express'
 import widgets from './routes/widgets.ts'
 
 const server = express()
 server.use(express.json())
-server.use(express.static(join(__dirname, 'public')))
+server.use(express.static(Path.join(__dirname, 'public')))
 
 server.use('/api/v1/widgets', widgets)
 
